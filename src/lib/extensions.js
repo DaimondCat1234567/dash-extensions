8eff489506f7a96a46877e9bf7835557a60b1ef9
DBDev <dambul2809@mail.ru>
2025-09-06 11:28:01 +0000

    Telegram Bot API extension banner change

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -28,7 +28,7 @@ const extensions = [
         id: "TelegramBotAPI",
         description: "Interact with Telegram API for Bots.",
         code: "damir2809/TelegramBotAPI.js",
-        banner: "damir2809/TelegramBotAPI.png",
+        banner: "damir2809/TelegramBotAPI.svg",
         creator: ["damir2809", "scratch_craft_2", "ttt999", "By-ROlil-CO", "AnonimKing24"],
         notes: "Translate to English soon.",
     },
